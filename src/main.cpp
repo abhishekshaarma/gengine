0bb196a799f480fa57e42a839a3113973d5c17b2
abhishekshaarma <abhishekshaarma2@gmail.com>
2025-12-23 12:51:27 -0800

    some shader

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <glm/glm.hpp>
-
+#include <string>
 #include "gl3d.h"
 #include "View.h"
 #include "SingletonView.h"
@@ -13,7 +13,53 @@
 #include "Material.h"
 #include "Light.h"
 
+static unsigned int CompileShader(unsigned int type, const std::string source)
+{
+    unsigned int id = glCreateShader(type);
+    // returns a pointer ti the begingi of the sring[] src = &source[0] basically
+    const char* src = source.c_str(); 
+    // if length is null each string is actually nullterminated 
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    //TODO: ERROR HANDLING 
+    int result;
+    // Query to see the shader file i -> interger v -> vector
+    glGetShaderiv(id , GL_COMPILE_STATUS, &result);
+    if(result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        //
+        char *message = (char*)alloca(length * sizeof(char)); // allocates on the stack dynamically(whoa!)
+        glGetShaderInfoLog(id,length, &length, message);
+        std::cout << "Failed to compile shader -> " <<
+            (type == GL_VERTEX_SHADER ? "vertex" : "fragmenet" ) << message << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+    
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader,const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram(); // returns an unsigned int 
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    return program;
+}
+
 GLuint vbo = 0;
+
 
 void init()
 {
@@ -50,7 +96,7 @@ void init()
     // -------------------------------
     // Create VBO ONCE
     // -------------------------------
-    float vertices[9] =
+    float position[9] =
         {
             0.0f, 0.0f, 1.0f,
             1.0f, 0.0f, 2.0f,
@@ -59,7 +105,7 @@ void init()
     
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(position), position, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
@@ -74,13 +120,43 @@ void display()
     mygllib::draw_axes();
 
     glPushMatrix();
-    glTranslatef(-2.5f, 0.0f, -1.5f);
 
     glEnableClientState(GL_VERTEX_ARRAY);
     
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0 , 2, GL_FLOAT, 0, sizeof(float) * 2, (const void*)0);
+
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout (location = 0) in vec4 position;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "\n"
+        "gl_Position = position;\n"
+        
+        "}\n";
+
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout (location = 0) out vec4 color;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "\n"
+        "color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        
+        "}\n";
+    
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
+
+    
     glDrawArrays(GL_TRIANGLES, 0, 3);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glDisableClientState(GL_VERTEX_ARRAY);
